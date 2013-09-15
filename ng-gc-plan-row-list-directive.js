bd81c7da130fe0c650bc6b3c90abbfc52c1971bd
Philip Harrison <philip@mailharrison.com>
2013-09-15 18:52:43 +0100

    Fix build

--- ng-gc-plan-row-list-directive.js
+++ ng-gc-plan-row-list-directive.js
@@ -5,50 +5,51 @@
  * License: MIT
  */
 (function(){
-  'use strict';
+'use strict';
 
-  angular.module('plan-row-list-template.html', []).run(['$templateCache', function($templateCache) {
-    $templateCache.put('plan-row-list-template.html',
-      '<div class="payments" ng-show="plans.length"><plan-row plan="plan" is-authorization="{{ !!plan.uri }}" url="{{ getPlanUrl(plan) }}" ng-repeat="plan in plans | orderBy:\'-created_at\'"></plan-row></div>');
-  }]);
+angular.module('plan-row-list-template.html', []).run(['$templateCache', function($templateCache) {
+  $templateCache.put('plan-row-list-template.html',
+    '<div class="payments" ng-show="plans.length"><plan-row plan="plan" is-authorization="{{ !!plan.uri }}" url="{{ getPlanUrl(plan) }}" ng-repeat="plan in plans | orderBy:\'-created_at\'"></plan-row></div>');
+}]);
 
-  'use strict';
+'use strict';
 
-  angular.module('gc.planRowListController', [])
-  .controller('PlanRowListController', [
-    '$scope',
-    function PlanRowListController($scope) {
+angular.module('gc.planRowListController', [])
+.controller('PlanRowListController', [
+  '$scope',
+  function PlanRowListController($scope) {
 
-      $scope.getPlanUrl = function getPlanUrl(plan) {
-        if (plan.uri) {
-          return plan.uri;
-        } else {
-          return '/plans/' + plan.id;
-        }
-      };
+    $scope.getPlanUrl = function getPlanUrl(plan) {
+      if (plan.uri) {
+        return plan.uri;
+      } else {
+        return '/plans/' + plan.id;
+      }
+    };
 
-    }
-  ]);
+  }
+]);
 
-  'use strict';
+'use strict';
 
-  angular.module('gc.planRowList', [
-    'gc.planRowListController',
-    'gc.planRow',
-    'plan-row-list-template.html'
-  ]).directive('planRowList',
-    [function planRowDirective() {
+angular.module('gc.planRowList', [
+  'gc.planRowListController',
+  'gc.planRow',
+  'plan-row-list-template.html'
+]).directive('planRowList', [
+  function planRowDirective() {
 
-      return {
-        restrict: 'E',
-        templateUrl: 'plan-row-list-template.html',
-        scope: {
-          plans: '=',
-          url: '@'
-        },
-        controller: 'PlanRowListController',
-        replace: true
-      };
+    return {
+      restrict: 'E',
+      templateUrl: 'plan-row-list-template.html',
+      scope: {
+        plans: '=',
+        url: '@'
+      },
+      controller: 'PlanRowListController',
+      replace: true
+    };
 
-    }]);
+  }
+]);
 })();
